5a99ab826c56f1c6690826a7d056712cbdb35b44
RaviKumar Mourya <mourya7537@gmail.com>
2025-08-11 20:58:52 +0530

    Schema Validation

--- schema.js
+++ schema.js
@@ -1,12 +1,15 @@
 const Joi = require("joi");
 
 module.exports.listingSchema = Joi.object({
-    listing : Joi.object({
-        title : Joi.string().required(),
-        decription: Joi.string().required(),
-        location : Joi.string().required(),
-        country : Joi.string().required(),
-        price : Joi.number().required().min(0),
-        image:Joi.string().allow("",null)
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.object({
+            filename: Joi.string(),
+            url: Joi.string().uri().required()
+        }).optional().allow(null)
     }).required()
 });
