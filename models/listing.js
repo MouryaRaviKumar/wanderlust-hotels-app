04f792b04f73db1815f46e1496ba7cba35a17197
RaviKumar Mourya <mourya7537@gmail.com>
2025-08-16 23:47:57 +0530

    Added Maps

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
+const { required } = require("joi");
 
 const listingSchema = new Schema({
     title: {
@@ -24,6 +25,19 @@ const listingSchema = new Schema({
     owner : {
         type : Schema.Types.ObjectId,
         ref : "User",
+    },
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true,
+            default: 'Point'
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            default: [78.2869, 14.8140]
+        }
     },
 });
 
---
+++ public/js/map.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: coordinates, 
+    zoom: 11
+});
+
+const marker = new mapboxgl.Marker({ color : "red"})
+    .setLngLat(coordinates)
+    .addTo(map);
