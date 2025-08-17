5421193a5520e893d9d2d874529027c8e08400fb
RaviKumar Mourya <mourya7537@gmail.com>
2025-08-17 16:36:23 +0530

    Final Commit

--- controller/listings.js
+++ controller/listings.js
@@ -42,7 +42,7 @@ module.exports.createListing = async (req, res, next) => {
     newListing.owner = req.user._id; 
     newListing.image = {url,filename};
     newListing.geometry = response.body.features[0].geometry;
-    let savedListing = await newListing.save();
+    await newListing.save();
     req.flash("success","New Listing Created!");
     res.redirect("/listings");
 };
