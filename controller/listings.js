35aad0d045f0ef67bf727b4053ffaa92d7b6ec88
RaviKumar Mourya <mourya7537@gmail.com>
2025-08-19 12:18:15 +0530

    fix(listings): Preserve image on update if no new file is uploaded

--- controller/listings.js
+++ controller/listings.js
@@ -64,13 +64,13 @@ module.exports.editListing = async (req, res, next) => {
 module.exports.updateListing = async (req, res, next) => {
     let { id } = req.params;
     let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    if(typeof req.file !== "undefined"){
+    if (req.file) {
         let url = req.file.path;
         let filename = req.file.filename;
-        listing.image = {url,filename};
+        listing.image = { url, filename }; 
         await listing.save();
     }
-    req.flash("success","Listing Updated!");
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 };
 
